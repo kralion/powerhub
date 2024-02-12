c7e1ec6bcf7cf470f7a0fc4d5abb397654687662
Brayan Paucar <joan300501@gmail.com>
2024-02-12 10:40:42 -0500

    Add Badge component to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { CreatePost } from "@/components/create-car";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
+import { Badge } from "@/components/ui/badge";
 
 export default async function Home() {
   const hello = await api.cars.hello.query({ text: "from tRPC" });
@@ -16,28 +17,41 @@ export default async function Home() {
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+            className="flex max-w-xs flex-col gap-4 rounded-xl border-2 border-white/10 bg-white/10 p-4 duration-300 hover:bg-white/20"
             href="/powerhub"
           >
             <h3 className="text-2xl font-bold">Dashboard →</h3>
             <div className="text-lg">
-              Here you can see the status of your power hub. And its
-              capabilities for your EV Car
+              Capabilities and status of the actual production release of{" "}
+              <Badge
+                className={
+                  " border-yellow-500 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
+                }
+              >
+                Powerhub v1.2.12
+              </Badge>
             </div>
           </Link>
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
+            className="flex max-w-xs flex-col gap-4 rounded-xl border-2 border-white/10 bg-white/10 p-4 duration-300 hover:bg-white/20"
+            href="https://github.com/Dv-Joan/powerhub"
             target="_blank"
           >
             <h3 className="text-2xl font-bold">Repository →</h3>
             <div className="text-lg">
-              Here you can see the releases of Powerhub and its new features,
-              contributions are welcome!
+              Releases of Powerhub and its new features, contributions are
+              welcome! on{" "}
+              <Badge
+                className={
+                  " border-slate-500 bg-gradient-to-r from-slate-400 via-zinc-500 to-black text-white"
+                }
+              >
+                Github
+              </Badge>
             </div>
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-2">
+        {/* <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
             {hello ? hello.greeting : "Loading tRPC query..."}
           </p>
@@ -53,7 +67,7 @@ export default async function Home() {
               {session ? "Sign out" : "Sign in"}
             </Link>
           </div>
-        </div>
+        </div> */}
         <CrudShowcase />
       </div>
     </main>
