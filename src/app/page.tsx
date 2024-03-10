33f0de6ce6651c2442d7f84b1b548898fddbcf60
Brayan <joan300501@gmail.com>
2024-03-10 11:15:19 -0500

    SaaS description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 import { CreatePost } from "@/components/create-car";
+import { Badge } from "@/components/ui/badge";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
-import { Badge } from "@/components/ui/badge";
-import { ChevronRight, MoveRight } from "lucide-react";
+import { MoveRight } from "lucide-react";
 
 export default async function Home() {
   const hello = await api.cars.hello.query({ text: "from tRPC" });
@@ -13,9 +13,16 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#8E2DE2] to-[#2B32B2] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight  sm:text-[5rem]">
-          Power<span className="text-[#30FFFF]">hub</span>
-        </h1>
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <h1 className="text-5xl font-extrabold tracking-tight  sm:text-[5rem]">
+            Power<span className="text-[#30FFFF]">hub</span>
+          </h1>
+          <span className="w-2/3 text-center text-gray-300 ">
+            Simplify EV management with our all-in-one solution, including
+            battery tracking and real-time GPS integration, perfect tool to
+            manage your EV Car.
+          </span>
+        </div>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
             className="group flex max-w-xs flex-col gap-4 rounded-xl border-2 border-white/10 bg-white/10 p-4 duration-300 hover:bg-white/20"
@@ -29,19 +36,20 @@ export default async function Home() {
               />
             </div>
             <div className="text-lg">
-              Capabilities and status of the actual production release of{" "}
+              Capabilities, versioning and status of the actual production
+              release of{" "}
               <Badge
                 className={
                   " border-yellow-500 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
                 }
               >
-                Powerhub v1.2.12
+                Release v 0.2.12
               </Badge>
             </div>
           </Link>
           <Link
             className="group flex max-w-xs flex-col gap-4 rounded-xl border-2 border-white/10 bg-white/10 p-4 duration-300 hover:bg-white/20"
-            href="https://github.com/Dv-Joan/powerhub"
+            href="https://github.com/kralion/powerhub"
             target="_blank"
           >
             <div className="flex items-center gap-2 text-2xl font-bold">
